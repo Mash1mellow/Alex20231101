376e838872b94250967f6d042bb8ea26a21f7927
Mash1mellow <alexander.rolind@gmail.com>
2023-11-01 14:49:31 +0800

    make it better

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    X = "作者:黃植達 20231101<br>"
+    X = "作者:黃植達 20231101b<br>"
     X += "<a href=/db>課程網頁</a><br>"
     X += "<a href=/alex?nick=alex>個人介紹及系統時間</a><br>"
     X += "<a href=/account>表單傳值</a><br>"
