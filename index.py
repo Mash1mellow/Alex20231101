86731c39c857f62e445b48e192aaa8125656a4bb
Mash1mellow <alexander.rolind@gmail.com>
2023-11-29 15:55:17 +0800

    make it better

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+import requests
+
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-
+from firebase_admin import credentials, firestore
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from datetime import datetime, timezone, timedelta
 
@@ -19,6 +21,7 @@ def index():
     X += "<br><a href=/read2>人選之人─造浪者</a><br>"
     X += "<br><a href=/read3>圖書精選</a><br>"
     X += "<br><a href=/search>Search</a><br>"
+    X += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
     return X
 
 @app.route("/db")
@@ -96,5 +99,36 @@ def read3():
         Result += "<img src = " + book["cover"] + ">" + "</img><br>"
     return Result
 
-#if __name__ == "__main__":
-    #app.run()
+@app.route("/movie")
+def movie():
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    lastUpdate = sp.find("div", class_="smaller09").text[5:]
+    for item in result:
+        picture = item.find("img").get("src").replace(" ", "")
+        title = item.find("div", class_="filmtitle").text
+        movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+        show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+        show = show.replace("片長：", "")
+        show = show.replace("分", "")
+        showDate = show[0:10]
+        showLength = show[13:]
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": showLength,
+            "lastUpdate": lastUpdate
+            }
+        db = firestore.client()
+        doc_ref = db.collection("電影").document(movie_id)
+        doc_ref.set(doc)
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+
+if __name__ == "__main__":
+    app.run(Debug=True)
--- spider.py
+++ spider.py
@@ -13,4 +13,13 @@ for x in result :
 	#print(x.find(class_="filmtitle").text)
 	print(x.find("img").get("src").replace(" ", "'"))
 	print("http://www.atmovies.com.tw/" + x.find("a").get("href"))
+	if x.find(class_="runtime").find("a") != None:
+		print(x.find(class_="runtime").find("a").text)
+	else: 
+		print(x.find(class_="runtime").text.replace("上映日期：", ""))
+
+	if x.find(class_="runtime").find("img") != None:
+		print("http://www.atmovies.com.tw" + x.find(class_="runtime").find("img").get("src"))
+	
+	print(x.find(class_="smaller09").text)
 	print()
